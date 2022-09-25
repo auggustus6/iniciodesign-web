37e97d1c6b01e7319fa2c5c0dd5b25ab90dc6d7e
guusfelix2015 <gusfelix2015@gmail.com>
2022-09-25 12:39:50 -0300

    Teste responsive menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,63 @@
-import Button from "../Button/Button";
 import styles from "./NavBar.module.css";
 import stylesButton from "../Button/Button.module.css";
+import Button from "../Button/Button";
 
-const NavBar = () => {
+import { useState } from "react";
+
+function NavBar() {
+  const [active, setActive] = useState(`${styles.navMenu}`);
+  const [icon, setIcon] = useState(`${styles.navToggler}`);
+
+  const navToggle = () => {
+    if (active === `${styles.navMenu}`) {
+      setActive(`${styles.navMenu} ${styles.navActive}`);
+    } else setActive(`${styles.navMenu}`);
+
+    // Icon Toggler
+    if (icon === `${styles.navMenu}`) {
+      setIcon(`${styles.navToggler} ${styles.toggle}`);
+    } else setIcon(`${styles.navToggler}`);
+  };
+
   return (
-    <nav className={styles.navBar}>
-      <h1>Logo</h1>
-      <ul className={styles.list}>
-        <li className={styles.item}>Home</li>
-        <li className={styles.item}>Serviços</li>
-        <li className={styles.item}>Sobre</li>
-        <li className={styles.item}>Portfólio</li>
-        <Button classname={stylesButton.btnPrimary}>Contato</Button>
+    <nav className={styles.nav}>
+      <a href="#" className={styles.navBrand}>
+        Logo
+      </a>
+      <ul className={active}>
+        <li className={styles.navItem}>
+          <a href="#" className={styles.navLink}>
+            Home
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="#" className={styles.navLink}>
+            Serviços
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="#" className={styles.navLink}>
+            Sobre
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="#" className={styles.navLink}>
+            Portfólio
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <Button classname={stylesButton.btnPrimary}>Contato</Button>
+          </a>
+        </li>
       </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className={styles.line1}></div>
+        <div className={styles.line2}></div>
+        <div className={styles.line3}></div>
+      </div>
     </nav>
   );
-};
+}
 
 export default NavBar;
