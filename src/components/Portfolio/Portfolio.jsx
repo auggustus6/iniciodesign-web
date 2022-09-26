4625240fc8b2c2e7f224508a21b396523dde7316
guusfelix2015 <gusfelix2015@gmail.com>
2022-09-25 23:26:04 -0300

    Slider mobile

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,20 @@
 import PortDesk from "../PortDesck/PortDesk";
+import Slider from "../Slider/Slider";
 import styles from "./Portfolio.module.css";
+import { useState, useEffect } from "react";
+import img1 from "../../assets/img1.svg";
+import img2 from "../../assets/img2.svg";
+import img3 from "../../assets/img3.svg";
 
 const Portfolio = () => {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 650);
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 650);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
   return (
     <div className={styles.bgContainer}>
       <div className={styles.container}>
@@ -11,7 +24,7 @@ const Portfolio = () => {
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint.
           </p>
-          <PortDesk />
+          {isDesktop ? <PortDesk /> : <Slider imgs={[img1, img2, img3]} />}
         </div>
       </div>
     </div>
