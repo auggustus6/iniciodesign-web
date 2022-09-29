69cdd533abc8ffb10544a7ace5c6d34587afea29
guusfelix2015 <gusfelix2015@gmail.com>
2022-09-28 22:21:08 -0300

    Fix container four section

--- src/components/FourSection/FourSection.jsx
+++ src/components/FourSection/FourSection.jsx
@@ -9,37 +9,35 @@ const FourSection = ({ text, paragraph }) => {
       <div className={styles.container}>
         <div className={styles.containerContent}>
           <div className={styles.content}>
-            <div className={styles.containerText}>
-              <h1>{text}</h1>
-              <p>{paragraph}</p>
-            </div>
-            <div className={styles.containerCard}>
-              <div className={styles.containerCircle}>
-                <div>
-                  <Circle
-                    tt="Amet minim"
-                    pp="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
-                  />
-                </div>
-                <div>
-                  <Circle
-                    tt="Amet minim"
-                    pp="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
-                  />
-                </div>
-                <div>
-                  <Circle
-                    tt="Amet minim"
-                    pp="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
-                  />
-                </div>
+            <h1>{text}</h1>
+            <p>{paragraph}</p>
+          </div>
+          <div className={styles.containerCard}>
+            <div className={styles.containerCircle}>
+              <div>
+                <Circle
+                  tt="Amet minim"
+                  pp="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+                />
               </div>
-              <img
-                className={styles.image}
-                src={developerApp}
-                alt="Imagem de duas pessoas ao lado de um smartphone"
-              />
+              <div>
+                <Circle
+                  tt="Amet minim"
+                  pp="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+                />
+              </div>
+              <div>
+                <Circle
+                  tt="Amet minim"
+                  pp="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+                />
+              </div>
             </div>
+            <img
+              className={styles.image}
+              src={developerApp}
+              alt="Imagem de duas pessoas ao lado de um smartphone"
+            />
           </div>
         </div>
       </div>
