049efa9c0d7a420d9b053db06877547dd44a70ac
guusfelix2015 <gusfelix2015@gmail.com>
2022-09-30 21:54:43 -0300

    Fix get imgs

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,11 @@
 import styles from "./Modal.module.css";
-import img1 from "../../assets/img1.svg";
 
-const Modal = ({ open, onClose, description, content, id }) => {
+const Modal = ({ open, onClose, description, content, id, img }) => {
   if (!open) return null;
   return (
     <div className={styles.overlay}>
       <div className={styles.modalContainer}>
-        <img src={img1} />
+        <img src={img} />
         <div className={styles.content}>
           <h1>{description}</h1>
           <h3>{content}</h3>
